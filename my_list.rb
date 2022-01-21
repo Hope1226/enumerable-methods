bba89a9482b9cf771a83deb6a0ab71fa35109ace
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-21 05:12:00 +0500

    Make final changes

--- my_list.rb
+++ my_list.rb
@@ -10,10 +10,3 @@ class MyList
     @list.map(&block)
   end
 end
-
-test = MyList.new([1, 2, 3, 4])
-test.each { |e| p e }
-
-puts(test.all? { |e| e < 5 })
-puts(test.any? { |e| e == 2 })
-p(test.filter(&:even?))
